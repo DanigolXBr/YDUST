ff8733a8470b35dd43718fdb55fc0cf9746d0090
Ana <anajuliax2017@gmail.com>
2022-06-14 21:53:08 -0300

    criação da pagina dos professores

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import Pg01Page from './pages/pg01';
 import Cadastro01 from './pages/Cadastro01';
 import Pgincial from './pages/Pgincial';
 import Cabecalho from './componentes/Cabecalho';
+import Pgprofessores from './pages/Pgprofessor';
 
 function App (){
   return (
     <>
       <Cabecalho/>
-      <Cadastro01/>
+      <Pgprofessores/>
     </>
   );
 };
---
+++ src/pages/Pgprofessor.tsx
@@ -0,0 +1,13 @@
+import './Pgprofessor.css';
+
+const Pgprofessores = function (){
+    return (
+        <>
+        <p>,
+            
+        </p>
+        </>
+    );
+};
+
+export default Pgprofessores;
