91f2df97911315c37afb72dac8cc8d282d7f1242
Ana <anajuliax2017@gmail.com>
2022-06-15 22:04:45 -0300

    Interface da tela Professor

--- src/pages/Pgprofessor.tsx
+++ src/pages/Pgprofessor.tsx
@@ -3,10 +3,16 @@ import './Pgprofessor.css';
 const Pgprofessores = function (){
     return (
         <>
-        <p>,
-            
-        </p>
+            <div className='barra'>
+                <button className='perfil'>Perfil</button>
+                <button className='redacao '>Redações</button>
+            </div>
+
+            <div className='Tema'>
+                <input type="text" placeholder='Tema' />
+            </div>
         </>
+        
     );
 };
 
