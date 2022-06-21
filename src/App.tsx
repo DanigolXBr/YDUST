cf2731a85499558ba4e944863dc99a712f618b21
Matheus <matheusop806@gmail.com>
2022-06-21 18:45:54 -0300

    att Matheus

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App (){
     <>
       <Cabecalho/>
       <Arame/>
-      <Pgincial/>
+      <Cadastro01/>
     </>
   );
 };
--- src/pages/Cadastro01.tsx
+++ src/pages/Cadastro01.tsx
@@ -9,7 +9,7 @@ enum Estado {
   inicial,
   
 };
-  
+
 const Cadastro01= function() {
       const [estado,setEstado] = useState (Estado.inicial);
 
@@ -17,17 +17,24 @@ const Cadastro01= function() {
         return (
          
             <div className='cadastro01'>
+            
             <img src={Img} alt="Livro" />
+              
               <div className='cadastro'>
                 <input type="text" placeholder='Nome de usuário' className='campo'/>
+                <br />
                 <input type="password" placeholder='Digite sua senha' className='campo'/>
+                <br />
                 <input type="text" placeholder='Digite seu Email' className='campo' />
+                <br />
                 <input type="text" placeholder='Digite seu CPF' className='campo' />
+                <br />
                 <input type="text" placeholder='Digite sua Data de Nascimento' className='campo'/>
                 <button className='salvar'>Salvar</button>
               </div>        
           </div>
     );
+
 }
 
 export default Cadastro01;
