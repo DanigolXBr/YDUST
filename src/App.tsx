b6a6b0dce040a3a81e61e01836b0bef85dfb705b
DanigolXBr <DanigolXBr.2020@gmail.com>
2022-06-24 22:04:56 -0300

    Mudança de Botao

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Cadastro01 from './pages/Cadastro01';
 import Pgincial from './pages/Pgincial';
 import Cabecalho from './componentes/Cabecalho';
 import Pg01Page from './pages/pg01';
+import Perfil from './pages/Perfil';
 
 
 
@@ -10,6 +11,7 @@ function App (){
   return (
     <>
       <Cabecalho/>
+      <Cadastro01/>
      
       
     </>
