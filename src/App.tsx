73cdc22a924676d67e43dcef81f0f55789e2dce6
DanigolXBr <DanigolXBr.2020@gmail.com>
2022-06-28 19:21:44 -0300

    [+] Rota para página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Cadastro01 from './pages/Cadastro01';
 import Cabecalho from './componentes/Cabecalho';
 import { Route, Routes } from 'react-router-dom';
 import Nredacao from './pages/Nredacao';
+import Pgincial from './pages/Pgincial';
 
-function App (){
+function App () {
   return (
     <>
         <Cabecalho/>
         <Routes>
+          <Route path="/" element={<Pgincial />} />
           <Route path='/cadastro' element={<Cadastro01 />} />
           <Route path='/nova-redacao' element={<Nredacao />} />
         </Routes>
